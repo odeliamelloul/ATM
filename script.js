7c2003bebd1e9940d9ef85ed8b5e61379ba398ea
odeliamelloul <59563137+odeliamelloul@users.noreply.github.com>
2021-08-04 22:24:29 +0300

    Update script.js
    changed limit of json length from 100 to 4000
    changed event of input search from "oninput" to "onkeyup"

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
-
 import axios from "./node_modules/@bundled-es-modules/axios/axios.js";
-import { DataTable} from "./node_modules/simple-datatables/dist/module/index.js"
+import {DataTable} from "./node_modules/simple-datatables/dist/module/index.js"
 const table = document.querySelector("#myTable");
 let selectorMapElement = document.querySelector("#gmap_canvas");
-
-const res = axios
-  .get(
-    "https://data.gov.il/api/3/action/datastore_search?resource_id=b9d690de-0a9c-45ef-9ced-3e5957776b26&limit=100"
-  )
-  .then((res) => {
+let dataTable
+ let allData
+const res = axios.get(  "https://data.gov.il/api/3/action/datastore_search?resource_id=b9d690de-0a9c-45ef-9ced-3e5957776b26&limit=4000")
+.then((res) => {
     const header = drawTitles(res.data.result.fields);
     tableBody.innerHTML = `<tr scope="row">${header}</tr>`;
-    drawTable(res.data.result.records);
-    drawOnMap(res.data.result.records);
-    const dataTable = new DataTable(table);
+    allData=res.data.result.records
+    drawTable(allData);
+    dataTable = new DataTable(table); 
   })
   .catch((err) => console.log(`error: ${err}`));
 
 let titles = document.querySelector("tr#table-titles");
 let tableBody = document.querySelector("tbody#table-body");
+
+const drawTitles = (allTitles) => {
+  let row = "";
+  allTitles.forEach((curTitle) => {
+    if(curTitle.id!=="ATM_Address_Extra")
+    row += `<th scope="col">${curTitle.id}</th>`;
+  });
+  titles.innerHTML = row;
+};
 
 const drawTable = (Content) => {
   let dataRows = "";
@@ -30,63 +36,31 @@ const drawTable = (Content) => {
 };
 
 const drawSingleRow = (rowProps) => {
+
   let row = "";
   for (let col in rowProps) {
-    row += `<td>${rowProps[col]}</td>`;
+    if(col!=="ATM_Address_Extra")
+    row += `<td class="${col}">${rowProps[col]}</td>`;
   }
   return row;
 };
 
-const drawTitles = (allTitles) => {
-  let row = "";
-  allTitles.forEach((curTitle) => {
-    row += `<th scope="col">${curTitle.id}</th>`;
-  });
-  titles.innerHTML = row;
-};
-
 //--------------------------google map--------------------------------
 
-const drawOnMap = (Content) => {
-  for (let i = 0; i < Content.length; i++) {
-    if (
-      isFinite(Content[i].X_Coordinate) &&
-      isFinite(Content[i].Y_Coordinate) &&
-      Content[i].X_Coordinate !== 0 &&
-      Content[i].Y_Coordinate !== 0
-    ) {
-      if (Content[i].X_Coordinate > 32 && Content[i].Y_Coordinate < 34) {
-        init_map(
-          Content[i].Bank_Name,
-          Content[i].Atm_Num,
-          Content[i].ATM_Address,
-          Content[i].Y_Coordinate,
-          Content[i].X_Coordinate
-        );
-      } else {
-        init_map(
-          Content[i].Bank_Name,
-          Content[i].Atm_Num,
-          Content[i].ATM_Address,
-          Content[i].X_Coordinate,
-          Content[i].Y_Coordinate
-        );
-      }
-    }
-  }
-};
-const myOptions = {
-  zoom: 8,
-  center: new google.maps.LatLng(31.4037193, 33.9606947),
+let myOptions = {
+  zoom: 7,
+  center: new google.maps.LatLng(32.4528759,37.9159274),
   mapTypeId: google.maps.MapTypeId.ROADMAP,
 };
-const map = new google.maps.Map(selectorMapElement, myOptions);
+let map = new google.maps.Map(selectorMapElement, myOptions);
+// google.maps.event.addDomListener(window, "load", init_map);
 
 function init_map(name,num, address, x, y) {
+  
   let infowindow = new google.maps.InfoWindow({
-    content: `
+        content: `
         <strong>שם: ${name} </strong>
-        <br> מספר: ${num}
+        <br> מספר : ${num}
         <br> כתובת: ${address} 
       `,
   });
@@ -98,5 +72,96 @@ function init_map(name,num, address, x, y) {
     infowindow.open(map, marker);
   });
 }
+// ------------------with dataTable.searchData------------
 
-google.maps.event.addDomListener(window, "load", init_map);
+
+setTimeout(function(){
+  let search = document.querySelectorAll(".dataTable-input")[0];
+
+  search.onkeyup = function(){
+    
+    document.querySelector("#gmap_canvas").innerHTML = "";
+    map = new google.maps.Map(selectorMapElement, myOptions);
+
+      for(var i=0;i<dataTable.searchData.length;i++)
+        drawOnMap(allData[dataTable.searchData[i]])
+
+       
+}}, 5000);
+
+
+const drawOnMap = (Content) => {
+  let x=Content["X_Coordinate"]
+  let y=Content["Y_Coordinate"]
+  let num = Content["Atm_Num"]
+  let name=Content["Bank_Name"]
+  let adress=Content["ATM_Address"]
+
+    if ( isFinite(x) &&isFinite(y) && x !== 0 && y !== 0 ) {
+      if (x > 32 && y < 34) {
+        init_map(name,num,adress,y,x)
+      } 
+      else {
+        init_map(name,num,adress,x,y)
+      }
+    }
+  
+};
+//------------------with dataTable.pages------------
+// let filter=[]
+
+// setTimeout(function(){
+//   let search = document.querySelectorAll(".dataTable-input")[0];
+//   search.onkeyup = function(){ 
+//     document.querySelector("#gmap_canvas").innerHTML = "";
+//     map = new google.maps.Map(selectorMapElement, myOptions);
+
+//     filter=[]
+
+//     for (let pageNum=0;pageNum<dataTable.pages.length;pageNum++)
+//     {
+//       for (var i=0;i<dataTable.pages[pageNum].length;i++)
+//         filter.push(dataTable.pages[0][i].cells)  
+//     }
+//     if(filter.length===100 && search.value!=="13")
+//      filter=[]
+
+//     for (var i=0;i<filter.length;i++){
+//        drawOnMap(filter[i]);
+//     }
+    
+// }}, 3000);
+
+// const drawOnMap = (Content) => {
+
+//   let x,y,name,num,adress
+
+//   for (let i = 0; i < Content.length; i++) {
+//     switch(Content[i].className)
+//     {
+//       case "X_Coordinate" :
+//          x=Content[i].innerText
+//          break;
+//       case "Y_Coordinate" :
+//           y=Content[i].innerText
+//           break;
+//       case "Bank_Name" :
+//           name=Content[i].innerText
+//           break;
+//       case "Atm_Num" :
+//            num=Content[i].innerText
+//             break;
+//       case "ATM_Address" :
+//             adress=Content[i].innerText
+//             break;
+//       default:
+//         break;
+//     }
+//   }
+//   if (x > 32 && y < 34) {
+//     init_map(name,num,adress,y,x);
+//   } else {
+//     init_map(name,num,adress,x,y);
+//   }
+
+// };
